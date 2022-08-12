8e43191d1fa3d2150fd1805ac2a9e00eb2362401
Saeedafshari11235 <msafshari11235@gmail.com>
2022-08-12 19:28:44 +0430

    adding cities and countries

---
+++ script.js
@@ -0,0 +1,6 @@
+let countries={
+    Iran:["Tehran","Mashhad","Esfahan","Shiraz","Kish"],
+    Turkey:["Istanbul","Antalya","Ankara","Erzurum","Mugla"],
+    US:["NewYork","Callifornia","Georgia","Texas","Arizona"],
+    Japan:["Tokyo","Osaka","Chiba","Hokaido","Hiroshima"]
+}
