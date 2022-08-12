6c9c899d36493b79bd7897e91469eabd8921a6e0
Saeedafshari11235 <msafshari11235@gmail.com>
2022-08-12 20:03:34 +0430

    adding city changer

--- script.js
+++ script.js
@@ -4,7 +4,31 @@ let countries={
     US:["NewYork","Callifornia","Georgia","Texas","Arizona"],
     Japan:["Tokyo","Osaka","Chiba","Hokaido","Hiroshima"]
 }
-let origin = document.querySelector("#orgn-cntr")
-origin.addEventListener("change",function(){
-    
+let originCountry = document.querySelector("#orgn-cntr")
+let originCity = document.querySelector("#orgn-ct")
+let destinationCountry = document.querySelector("#dst-cntr")
+let destinationCity = document.querySelector("#dst-ct")
+originCountry.addEventListener("change",function(){
+  if(originCountry.value!="select"){
+    originCity.innerHTML="Select one..."
+    countries[originCountry.value].forEach(element => {
+        let option = document.createElement("option")
+        option.innerHTML=element
+        originCity.appendChild(option)
+    });
+  }else{
+    originCity.innerHTML ="<option>Select one...</option>"
+  }
 })
+destinationCountry.addEventListener("change",function(){
+    if(destinationCountry.value!="select"){
+      destinationCity.innerHTML="Select one..."
+      countries[destinationCountry.value].forEach(element => {
+          let option = document.createElement("option")
+          option.innerHTML=element
+          destinationCity.appendChild(option)
+      });
+    }else{
+        destinationCity.innerHTML ="<option>Select one...</option>"
+    }
+  })
